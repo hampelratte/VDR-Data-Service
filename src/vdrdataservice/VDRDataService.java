a29aa205eb740fe4a2f977d04c7d3cc3e6d5eabc
Henrik Niehaus <henrik.niehaus@gmx.de>
2012-02-04 18:53:17 +0100

    - Fix: the plugin requested the EPG for a wrong time interval, which resulted in inconsitent data in the TVB schedule - increased version number to 0.5.3 - code formatting

--- src/vdrdataservice/VDRDataService.java
+++ src/vdrdataservice/VDRDataService.java
@@ -28,7 +28,6 @@ import org.hampelratte.svdrp.responses.highlevel.DVBChannel;
 import org.hampelratte.svdrp.responses.highlevel.EPGEntry;
 import org.hampelratte.svdrp.responses.highlevel.PvrInputChannel;
 
-
 import tvdataservice.MutableChannelDayProgram;
 import tvdataservice.MutableProgram;
 import tvdataservice.SettingsPanel;
@@ -45,35 +44,36 @@ import devplugin.ProgressMonitor;
 import devplugin.Version;
 
 /**
- * @author <a href="hampelratte@users.sf.net">hampelratte@users.sf.net </a>
+ * @author <a href="hampelratte@users.sf.net">hampelratte@users.sf.net</a>
  * 
  */
 public class VDRDataService extends AbstractTvDataService {
-	
-	private static Logger logger = Logger.getLogger(VDRDataService.class.getName());
 
-	private Localizer localizer = Localizer.getLocalizerFor(VDRDataService.class);
-  
-	private VDRDataServiceSettingsPanel settingsPanel;
+    private static Logger logger = Logger.getLogger(VDRDataService.class.getName());
+
+    private final Localizer localizer = Localizer.getLocalizerFor(VDRDataService.class);
+
+    private VDRDataServiceSettingsPanel settingsPanel;
 
     private Channel[] channels = new Channel[] {};
 
-    private Properties props = new Properties();
-    
-    private ChannelGroup cg = new ChannelGroupImpl("vdr", "VDR", "Channels from VDR", "VDR");
+    private final Properties props = new Properties();
 
-    private PluginInfo pluginInfo = new PluginInfo(
-    		VDRDataService.class,
-    		"VDR DataService",
-    		localizer.msg("desc", "Loads the EPG-Data from VDR (by Klaus Schmidinger) into TV-Browser"),
+    private final ChannelGroup cg = new ChannelGroupImpl("vdr", "VDR", "Channels from VDR", "VDR");
+
+    private final PluginInfo pluginInfo = new PluginInfo(
+            VDRDataService.class,
+            "VDR DataService",
+            localizer.msg("desc", "Loads the EPG-Data from VDR (by Klaus Schmidinger) into TV-Browser"),
             "Henrik Niehaus (hampelratte@users.sf.net)");
-    
 
+
+    @Override
     public void updateTvData(TvDataUpdateManager database, Channel[] channels,
             devplugin.Date date, int dateCount, ProgressMonitor pm)
-            throws TvBrowserException {
-        
-    	pm.setMaximum(channels.length);
+                    throws TvBrowserException {
+
+        pm.setMaximum(channels.length);
         if(channels.length > 0) {
             Connection conn = null;
             try {
@@ -88,10 +88,10 @@ public class VDRDataService extends AbstractTvDataService {
                         MutableChannelDayProgram[] dayPrograms = parseData(data, channels[i], date, dateCount);
                         pm.setMessage(localizer.msg("updating_database","Updating EPG database"));
                         for (int j = 0; j < dayPrograms.length; j++) {
-                        	if(dayPrograms[j].getProgramCount() > 0) {
-                        		database.updateDayProgram(dayPrograms[j]);
-                        		logger.info("Adding mutable program: " + dayPrograms[j]);
-                        	}
+                            if(dayPrograms[j].getProgramCount() > 0) {
+                                logger.info("Adding mutable program: " + dayPrograms[j]);
+                                database.updateDayProgram(dayPrograms[j]);
+                            }
                         }
                     } else {
                         StringBuffer sb = new StringBuffer(channels[i].getName());
@@ -103,15 +103,15 @@ public class VDRDataService extends AbstractTvDataService {
                         }
                         pm.setMessage(sb.toString());
                     }
-                    
+
                     pm.setValue(i);
                 }
             } catch (Exception e) {
                 logger.severe(e.getLocalizedMessage());
-                JOptionPane.showMessageDialog(getParentFrame(), 
-                        localizer.msg("couldnt_connect", "<html>Couldn't connect to VDR<br>{0}</html>",  
-                                e.getClass().getSimpleName() + ": " + e.getLocalizedMessage()), 
-                        Localizer.getLocalization(Localizer.I18N_ERROR), JOptionPane.ERROR_MESSAGE);
+                JOptionPane.showMessageDialog(getParentFrame(),
+                        localizer.msg("couldnt_connect", "<html>Couldn't connect to VDR<br>{0}</html>",
+                                e.getClass().getSimpleName() + ": " + e.getLocalizedMessage()),
+                                Localizer.getLocalization(Localizer.I18N_ERROR), JOptionPane.ERROR_MESSAGE);
             } finally {
                 if(conn != null) {
                     try {
@@ -129,16 +129,23 @@ public class VDRDataService extends AbstractTvDataService {
         List<MutableChannelDayProgram> dayProgramList = new ArrayList<MutableChannelDayProgram>();
         MutableChannelDayProgram dayProgram = null;
         MutableProgram program = null;
-        
+
         List<EPGEntry> entries = new EPGParser().parse(data);
         for (int i = 0; i < dateCount; i++) {
             Calendar start = date.getCalendar();
-            start .add(Calendar.DAY_OF_MONTH, i);
+            start.add(Calendar.DAY_OF_MONTH, i);
+            start.set(Calendar.HOUR_OF_DAY, 0);
+            start.set(Calendar.MINUTE, 0);
+            start.set(Calendar.SECOND, 0);
             Calendar stop = date.getCalendar();
-            stop .add(Calendar.DAY_OF_MONTH, i + 1);
+            stop.add(Calendar.DAY_OF_MONTH, i + 1);
+            stop.set(Calendar.HOUR_OF_DAY, 0);
+            stop.set(Calendar.MINUTE, 0);
+            stop.set(Calendar.SECOND, 0);
             Date currentDate = new Date(start);
-            logger.fine("Parsing program for " + channel.getName() + " from " + 
-                    DateFormat.getDateTimeInstance().format(start.getTime()) + 
+            logger.fine("##############################\nParsing program for " + channel.getName() + " from "
+                    +
+                    DateFormat.getDateTimeInstance().format(start.getTime()) +
                     " to " +
                     DateFormat.getDateTimeInstance().format(stop.getTime()));
 
@@ -146,24 +153,26 @@ public class VDRDataService extends AbstractTvDataService {
             for (EPGEntry entry : entries) {
                 if ((entry.getStartTime().after(start) && entry.getEndTime().before(stop))
                         || entry.getStartTime().get(Calendar.DAY_OF_MONTH) == start.get(Calendar.DAY_OF_MONTH)) {
-                    program = new MutableProgram(channel, new Date(entry.getStartTime()), 
-                            entry.getStartTime().get(Calendar.HOUR_OF_DAY), 
+                    program = new MutableProgram(channel, new Date(entry.getStartTime()),
+                            entry.getStartTime().get(Calendar.HOUR_OF_DAY),
                             entry.getStartTime().get(Calendar.MINUTE), false);
                     program.setTitle(entry.getTitle());
                     program.setShortInfo(entry.getDescription());
                     program.setDescription(entry.getDescription());
                     dayProgram.addProgram(program);
+                    logger.fine("Program: " + entry.getTitle() + " " + entry.getStartTime().getTime() + "-" + entry.getEndTime().getTime());
                 }
             }
             dayProgramList.add(dayProgram);
         }
-        
+
         MutableChannelDayProgram[] progs = new MutableChannelDayProgram[] {};
-        progs = (MutableChannelDayProgram[]) dayProgramList.toArray(progs);
+        progs = dayProgramList.toArray(progs);
         return progs;
     }
 
 
+    @Override
     public void loadSettings(Properties p) {
         // set defaults
         props.setProperty("vdr.host", "htpc");
@@ -178,50 +187,50 @@ public class VDRDataService extends AbstractTvDataService {
             String key = (String) en.nextElement();
             props.setProperty(key, p.getProperty(key));
         }
-        
+
         // create channel[]
         en = props.keys();
         while(en.hasMoreElements()) {
             String key = (String)(en.nextElement());
-	        if(key.startsWith("CHANNELID")) {
-	            String id = getChannelID(key);
-	            if(!id.equals("")) {
-	                String channelID = props.getProperty("CHANNELID"+id);
-	                String name = props.getProperty("CHANNELNAME"+id);
-	                name = name == null ? "Channel("+id+")" : name;
-	                String url = props.getProperty("CHANNELURL"+id);
-	                url = url == null ? "" : url;
-	                String country = props.getProperty("CHANNELCOUNTRY"+id);
-	                country = country == null ? Locale.getDefault().getCountry() : country;
-	                String copy = props.getProperty("CHANNELCOPYRIGHT"+id);
-	                int category = 0;
-	                try {
-						category = Integer.parseInt(props.getProperty("CHANNELCATEGORY"+id));
-					} catch (NumberFormatException e) {}
-	                copy = copy == null ? "" : copy;
-	                Channel chan = new Channel(this, name, channelID, TimeZone.getDefault(), "de", "", "", cg, null, category); 
-	                	//new Channel(this, name, channelID, TimeZone.getDefault(), country, copy, "", cg);
-	                list.add(chan);
-	            }
-	        }
+            if(key.startsWith("CHANNELID")) {
+                String id = getChannelID(key);
+                if(!id.equals("")) {
+                    String channelID = props.getProperty("CHANNELID"+id);
+                    String name = props.getProperty("CHANNELNAME"+id);
+                    name = name == null ? "Channel("+id+")" : name;
+                    String url = props.getProperty("CHANNELURL"+id);
+                    url = url == null ? "" : url;
+                    String country = props.getProperty("CHANNELCOUNTRY"+id);
+                    country = country == null ? Locale.getDefault().getCountry() : country;
+                    String copy = props.getProperty("CHANNELCOPYRIGHT"+id);
+                    int category = 0;
+                    try {
+                        category = Integer.parseInt(props.getProperty("CHANNELCATEGORY"+id));
+                    } catch (NumberFormatException e) {}
+                    copy = copy == null ? "" : copy;
+                    Channel chan = new Channel(this, name, channelID, TimeZone.getDefault(), "de", "", "", cg, null, category);
+                    //new Channel(this, name, channelID, TimeZone.getDefault(), country, copy, "", cg);
+                    list.add(chan);
+                }
+            }
         }
-        
+
         channels = (Channel[])list.toArray(channels);
 
         VDRConnection.host = props.getProperty("vdr.host");
         VDRConnection.port = Integer.parseInt(props.getProperty("vdr.port"));
         VDRConnection.charset = props.getProperty("charset") == null ? "ISO-8859-1" : props.getProperty("charset");
     }
-    
-    
+
+
     public String getProperty(String key) {
-      return props.getProperty(key);
+        return props.getProperty(key);
     }
-    
+
     public void setProperty(String key, String value) {
-      props.setProperty(key, value);
+        props.setProperty(key, value);
     }
-    
+
     private String getChannelID(String s) {
         Pattern p = Pattern.compile("^([A-Z]+)(\\d+)$");
         Matcher m = p.matcher(s);
@@ -232,11 +241,12 @@ public class VDRDataService extends AbstractTvDataService {
         return "";
     }
 
+    @Override
     public Properties storeSettings() {
-    	// delete old channels
-    	sweepOffChannelsFromProps();
-    	
-    	// save the current channels
+        // delete old channels
+        sweepOffChannelsFromProps();
+
+        // save the current channels
         for (int i = 0; i < channels.length; i++) {
             if (channels[i].getId() != null) {
                 props.setProperty("CHANNELID" + i, channels[i].getId());
@@ -251,9 +261,9 @@ public class VDRDataService extends AbstractTvDataService {
                 props.setProperty("CHANNELCOUNTRY" + i, channels[i]
                         .getCountry());
             }
-            
+
             props.setProperty("CHANNELCATEGORY" + i, ""+channels[i].getCategories());
-            
+
             if (channels[i].getCopyrightNotice() != null) {
                 props.setProperty("CHANNELCOPYRIGHT" + i, channels[i]
                         .getCopyrightNotice());
@@ -263,18 +273,20 @@ public class VDRDataService extends AbstractTvDataService {
     }
 
     private void sweepOffChannelsFromProps() {
-		for (Iterator iterator = props.keySet().iterator(); iterator.hasNext();) {
-			String key = (String) iterator.next();
-			if(key.startsWith("CHANNEL")) {
-				iterator.remove();
-			}
-		}
-	}
+        for (Iterator iterator = props.keySet().iterator(); iterator.hasNext();) {
+            String key = (String) iterator.next();
+            if(key.startsWith("CHANNEL")) {
+                iterator.remove();
+            }
+        }
+    }
 
-	public boolean hasSettingsPanel() {
+    @Override
+    public boolean hasSettingsPanel() {
         return true;
     }
-    
+
+    @Override
     public SettingsPanel getSettingsPanel() {
         if(settingsPanel == null) {
             settingsPanel = new VDRDataServiceSettingsPanel();
@@ -284,29 +296,34 @@ public class VDRDataService extends AbstractTvDataService {
         return settingsPanel;
     }
 
+    @Override
     public boolean supportsDynamicChannelList() {
         return true;
     }
 
+    @Override
     public PluginInfo getInfo() {
-    	return pluginInfo;
+        return pluginInfo;
     }
 
+    @Override
     public ChannelGroup[] getAvailableGroups() {
         ChannelGroup[] cgs = new ChannelGroup[] {cg};
         return cgs;
     }
-    
 
+
+    @Override
     public Channel[] getAvailableChannels(ChannelGroup cg) {
         if(cg.equals(this.cg)) {
-        	logger.info("Returning " + channels.length + " channels");
-        	return this.channels;
+            logger.info("Returning " + channels.length + " channels");
+            return this.channels;
         } else {
             return new Channel[] {};
         }
     }
 
+    @Override
     public Channel[] checkForAvailableChannels(ChannelGroup cg, ProgressMonitor pm) throws TvBrowserException {
         if (cg.getId().equals(this.cg.getId())) {
             pm.setMessage(localizer.msg("getting_data", "Getting data from VDR..."));
@@ -346,20 +363,20 @@ public class VDRDataService extends AbstractTvDataService {
             return new Channel[] {};
         }
     }
-    
+
     private int getChannelCategory(org.hampelratte.svdrp.responses.highlevel.Channel c) {
         if(c instanceof DVBChannel) {
             DVBChannel chan = (DVBChannel) c;
-        	String vpid = chan.getVPID();
-        	if("0".equals(vpid) || "1".equals(vpid)) { // a radio station
-        		return Channel.CATEGORY_RADIO;
-        	} else { // a tv channel
-        		if(!"0".equals(chan.getConditionalAccess())) {
-        			return Channel.CATEGORY_TV | Channel.CATEGORY_PAY_TV;
-        		} else {
-        			return Channel.CATEGORY_TV | Channel.CATEGORY_DIGITAL;
-        		}
-        	}
+            String vpid = chan.getVPID();
+            if("0".equals(vpid) || "1".equals(vpid)) { // a radio station
+                return Channel.CATEGORY_RADIO;
+            } else { // a tv channel
+                if(!"0".equals(chan.getConditionalAccess())) {
+                    return Channel.CATEGORY_TV | Channel.CATEGORY_PAY_TV;
+                } else {
+                    return Channel.CATEGORY_TV | Channel.CATEGORY_DIGITAL;
+                }
+            }
         } else if (c instanceof PvrInputChannel) {
             return Channel.CATEGORY_TV;
         } else {
@@ -367,17 +384,20 @@ public class VDRDataService extends AbstractTvDataService {
         }
     }
 
+    @Override
     public ChannelGroup[] checkForAvailableChannelGroups(ProgressMonitor arg0) throws TvBrowserException {
         return getAvailableGroups();
     }
 
+    @Override
     public boolean supportsDynamicChannelGroups() {
         return false;
     }
 
-	public void setWorkingDirectory(File dataDir) {}
+    @Override
+    public void setWorkingDirectory(File dataDir) {}
 
-	public static Version getVersion() {
-		return new Version(0,52);
-	}
+    public static Version getVersion() {
+        return new Version(0, 53);
+    }
 }
